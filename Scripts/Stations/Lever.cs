eab43c6f38d54d606e789b386f24ee62b2c77ae9
joshuaclarkuk <66115393+joshuaclarkuk@users.noreply.github.com>
2024-10-01 14:12:56 +0100

    Refactored Lever Behaviour
    Brings it in line with Card behaviour for easier debugging

--- Scripts/Stations/Lever.cs
+++ Scripts/Stations/Lever.cs
@@ -10,6 +10,7 @@ public partial class Lever : Node3D
     private float startRotation = Mathf.DegToRad(-45.0f);
     private float targetRotation = Mathf.DegToRad(45.0f);
     private float currentRotation = 0.0f;
+    private float range = 0.0f;
 
     public event Action OnLeverTargetReached;
     public event Action OnLeverTargetLeft;
@@ -21,23 +22,64 @@ public partial class Lever : Node3D
         targetRotation = Mathf.DegToRad(desiredTargetRotation);
         currentRotation = startRotation;
 
+        range = Mathf.Abs(startRotation - targetRotation);
+
         // Make sure lever starts in correct position
-        Rotation = new Vector3(currentRotation, Rotation.Y, Rotation.Z);
+        UpdateRotation(currentRotation);
     }
 
     // Be able to drag a lever down using mouse drag
     public void MovePhysicalHandleWithMouseMotion(float mouseDragSensitivity, Vector2 mouseDragMotion)
     {
-        float range = startRotation - targetRotation;
-        float percentageStep = (range * mouseDragSensitivity) * mouseDragMotion.Y;
-        currentRotation += percentageStep;
-        currentRotation = Mathf.Clamp(currentRotation, startRotation, targetRotation);
+        float percentageStep = mouseDragMotion.Y * (range * mouseDragSensitivity);
 
-        Rotation = new Vector3(currentRotation, Rotation.Y, Rotation.Z);
+        if (startRotation > targetRotation)
+        {
+            currentRotation += percentageStep;
+            currentRotation = Mathf.Clamp(currentRotation, startRotation, targetRotation);
+        }
+        else
+        {
+            currentRotation -= percentageStep;
+            currentRotation = Mathf.Clamp(currentRotation, startRotation, targetRotation);
+        }
+
+        UpdateRotation(currentRotation);
 
         if (currentRotation >= targetRotation * 0.9f)
         {
             GD.Print("Lever at end of motion");
+        }
+    }
+
+    private void UpdateRotation(float newRotation)
+    {
+        Rotation = new Vector3(newRotation, Rotation.Y, Rotation.Z);
+
+        float ninetyPercentPoint = 0.0f;
+        if (startRotation > targetRotation)
+        {
+            ninetyPercentPoint = targetRotation + (range * 0.1f);  // 90% towards the target (downwards)
+            if (currentRotation <= ninetyPercentPoint)
+            {
+                OnLeverTargetReached?.Invoke();
+            }
+            else
+            {
+                OnLeverTargetLeft?.Invoke();
+            }
+        }
+        else
+        {
+            ninetyPercentPoint = targetRotation - (range * 0.1f);  // 90% towards the target (upwards)
+            if (currentRotation >= ninetyPercentPoint)
+            {
+                OnLeverTargetReached?.Invoke();
+            }
+            else
+            {
+                OnLeverTargetLeft?.Invoke();
+            }
         }
     }
 }
