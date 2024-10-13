0fe90f2845c4b6b13d3b31f37d09a87628fe1cfe
joshuaclarkuk <66115393+joshuaclarkuk@users.noreply.github.com>
2024-10-13 07:19:37 +0100

    Feeding Request Sprites
    Started working on using Eldritch sprites as feeding request symbols. Will need to attach to buttons too

---
+++ Scripts/Stations/FeedingStation/FoodSprites/EldritchSprite.cs
@@ -0,0 +1,7 @@
+﻿using Godot;
+
+public abstract partial class EldritchSprite : Sprite3D
+{
+    [ExportCategory("Identifying Enum")]
+    [Export] public E_IngredientList IngredientType { get; private set; }
+}
