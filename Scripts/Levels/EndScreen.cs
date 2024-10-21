a40949f65299a99aacdf0d520ee553173a6a2070
joshuaclarkuk <66115393+joshuaclarkuk@users.noreply.github.com>
2024-10-21 13:53:56 +0100

    Ending Text Populated
    Now have ending text for each of the endings.

--- Scripts/Levels/EndScreen.cs
+++ Scripts/Levels/EndScreen.cs
@@ -5,6 +5,7 @@ public partial class EndScreen : Control
 {
     [ExportCategory("Required Nodes")]
     [Export] private Label endingTextNode = null;
+    [Export] private Button quitButtonNode = null;
 
     [ExportCategory("Ending Text")]
     [Export(PropertyHint.MultilineText)] private string firedEndingText = null;
@@ -14,17 +15,31 @@ public partial class EndScreen : Control
     private GlobalValues globalValues = null;
     private Player player = null;
 
+    private float percentageIncrement = 0.01f;
+    private float elapsedTime = 0.0f;
+    private float updateInterval = 0.02f; // Time in seconds between updates
+
     public override void _Ready()
     {
         globalValues = GetNode<GlobalValues>("/root/GlobalValues");
         player = GetNode<Player>("/root/Player");
 
+        // Link quit button signals
+        quitButtonNode.Pressed += HandleQuitButtonPressed;
+
         player.QueueFree();
         PlayEndingScroll();
     }
 
+    public override void _Process(double delta)
+    {
+        RevealText(delta);
+    }
+
     private void PlayEndingScroll()
     {
+        endingTextNode.VisibleRatio = 0.0f;
+
         switch (globalValues.EndState)
         {
             case E_EndState.NONE:
@@ -43,5 +58,35 @@ public partial class EndScreen : Control
                 endingTextNode.Text = creatureWinEndingText;
                 break;
         }
+    }
+
+    private void RevealText(double delta)
+    {
+        elapsedTime += (float)delta;
+
+        if (elapsedTime >= updateInterval)
+        {
+            elapsedTime = 0.0f;
+
+            if (endingTextNode.VisibleRatio < 1.0f)
+            {
+                endingTextNode.VisibleRatio += percentageIncrement;
+
+                if (endingTextNode.VisibleRatio > 1.0f)
+                {
+                    endingTextNode.VisibleRatio = 1.0f;  // Clamp the value to 1.0
+                }
+            }
+            else
+            {
+                // Stop the process loop when the text is fully revealed
+                SetProcess(false);
+            }
+        }
+    }
+
+    private void HandleQuitButtonPressed()
+    {
+        GetTree().Quit();
     }
 }
